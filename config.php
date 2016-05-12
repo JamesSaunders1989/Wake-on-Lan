bd525deeb9b5bd9d3a15de3d98bb32fb5bfbf4f5
JamesSaunders1989 <james.saunders1989@gmail.com>
2016-05-12 18:31:27 +0100

    Wake on Lan
    Wake a PC from raspberry pi + remote force shutdown.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+	/*
+	Author: James Saunders
+	*/
+
+	$USE_HTTPS = false;
+
+	$APPROVED_HASH = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+
+	$MAX_PINGS = 15;
+
+	$SLEEP_TIME = 5;
+	
+	$COMPUTER_SLEEP_CMD_PORT = 7760;
+
+	$COMPUTER_SLEEP_CMD = "suspend";
+
+	$BOOTSTRAP_LOCATION_PREFIX = "";
+
+	$COMPUTER_NAME = array("computer1","computer2");
+
+	$COMPUTER_MAC = array("00:00:00:00:00:00","00:00:00:00:00:00");
+
+	$COMPUTER_LOCAL_IP = array("192.168.0.1","192.168.0.2");
+
+
+
+?>
